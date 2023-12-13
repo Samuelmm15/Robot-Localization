6b7bab4b72bc1d7d93fa11d98c64864a08c8bf1e
Samuel <alu01013595262ull.edu.es>
2023-12-13 09:13:42 +0000

    Started the implementation of the localization practice

---
+++ Scripts_localizacion/localizacion.py
@@ -0,0 +1,185 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Rob�tica Computacional 
+# Grado en Ingenier�a Inform�tica (Cuarto)
+# Pr�ctica 5:
+#     Simulaci�n de robots m�viles holon�micos y no holon�micos.
+
+# localizacion.py
+
+# Samuel Martín Morales
+
+import sys
+from math import *
+from robot import robot
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+# ******************************************************************************
+# Declaraci�n de funciones
+
+def distancia(a,b):
+  # Distancia entre dos puntos (admite poses)
+  return np.linalg.norm(np.subtract(a[:2],b[:2]))
+
+def angulo_rel(pose,p):
+  # Diferencia angular entre una pose y un punto objetivo 'p'
+  w = atan2(p[1]-pose[1],p[0]-pose[0])-pose[2]
+  while w >  pi: w -= 2*pi
+  while w < -pi: w += 2*pi
+  return w
+
+def mostrar(objetivos,ideal,trayectoria):
+  # Mostrar objetivos y trayectoria:
+  #plt.ion() # modo interactivo
+  # Fijar los bordes del gr�fico
+  objT   = np.array(objetivos).T.tolist()
+  trayT  = np.array(trayectoria).T.tolist()
+  ideT   = np.array(ideal).T.tolist()
+  bordes = [min(trayT[0]+objT[0]+ideT[0]),max(trayT[0]+objT[0]+ideT[0]),
+            min(trayT[1]+objT[1]+ideT[1]),max(trayT[1]+objT[1]+ideT[1])]
+  centro = [(bordes[0]+bordes[1])/2.,(bordes[2]+bordes[3])/2.]
+  radio  = max(bordes[1]-bordes[0],bordes[3]-bordes[2])*.75
+  plt.xlim(centro[0]-radio,centro[0]+radio)
+  plt.ylim(centro[1]-radio,centro[1]+radio)
+  # Representar objetivos y trayectoria
+  idealT = np.array(ideal).T.tolist()
+  plt.plot(idealT[0],idealT[1],'-g')
+  plt.plot(trayectoria[0][0],trayectoria[0][1],'or')
+  r = radio * .1
+  for p in trayectoria:
+    plt.plot([p[0],p[0]+r*cos(p[2])],[p[1],p[1]+r*sin(p[2])],'-r')
+    #plt.plot(p[0],p[1],'or')
+  objT   = np.array(objetivos).T.tolist()
+  plt.plot(objT[0],objT[1],'-.o')
+  plt.show()
+  input()
+  plt.clf()
+
+def localizacion(balizas, real, ideal, centro, radio, mostrar=0):
+  # Buscar la localizaci�n m�s probable del robot, a partir de su sistema
+  # sensorial, dentro de una regi�n cuadrada de centro "centro" y lado "2*radio".
+
+  # Hay que hacer una búsqueda del robot real por el entorno
+  # Hay que tener en cuenta que cuando se haga una búsqueda, el tamaño de las divisiones
+  # debe ser mayor que el radio del robot, para que no se pierda en el entorno, ya que si se
+  # pierde, no se podrá localizar o será más complicado de localizar
+
+  # Cuando se detecta que el robot se ha desviado a partir del real, se aplica la localización
+  # y la búsqueda se encarga de analizar cada uno de los pixeles de la imagen para ver si se
+  # encuentra el robot real para poder enviarlo a la posición del robot ideal.
+
+  # Para que el robot se ejecuta de manera correcta, se deben de tener al menos dos
+  # balizas, ya que si tiene una baliza, no se puede calcular la posición del robot real
+  # y por tanto el robot se perderá seguramente.
+
+
+
+  if mostrar:
+    #plt.ion() # modo interactivo
+    plt.xlim(centro[0]-radio,centro[0]+radio)
+    plt.ylim(centro[1]-radio,centro[1]+radio)
+    imagen.reverse()
+    plt.imshow(imagen,extent=[centro[0]-radio,centro[0]+radio,\
+                              centro[1]-radio,centro[1]+radio])
+    balT = np.array(balizas).T.tolist();
+    plt.plot(balT[0],balT[1],'or',ms=10)
+    plt.plot(ideal.x,ideal.y,'D',c='#ff00ff',ms=10,mew=2)
+    plt.plot(real.x, real.y, 'D',c='#00ff00',ms=10,mew=2)
+    plt.show()
+    input()
+    plt.clf()
+
+# ******************************************************************************
+
+# La orientación que se puede observar abajo se refiere al ángulo
+# Definici�n del robot:
+P_INICIAL = [0.,4.,0.] # Pose inicial (posici�n y orientacion)
+V_LINEAL  = .7         # Velocidad lineal    (m/s)
+V_ANGULAR = 140.       # Velocidad angular   (�/s)
+FPS       = 10.        # Resoluci�n temporal (fps)
+
+HOLONOMICO = 1
+GIROPARADO = 0
+LONGITUD   = .2
+
+# Estas trayectorias se pueden definir como se quiera
+# Estos valores pueden ser cambiados
+# Definici�n de trayectorias:
+trayectorias = [
+    [[1,3]],
+    [[0,2],[4,2]],
+    [[2,4],[4,0],[0,0]],
+    [[2,4],[2,0],[0,2],[4,2]],
+    [[2+2*sin(.8*pi*i),2+2*cos(.8*pi*i)] for i in range(5)]
+    ]
+
+# Definici�n de los puntos objetivo:
+if len(sys.argv)<2 or int(sys.argv[1])<0 or int(sys.argv[1])>=len(trayectorias):
+  sys.exit(sys.argv[0]+" <indice entre 0 y "+str(len(trayectorias)-1)+">")
+objetivos = trayectorias[int(sys.argv[1])]
+
+# Definici�n de constantes:
+EPSILON = .1                # Umbral de distancia
+V = V_LINEAL/FPS            # Metros por fotograma
+W = V_ANGULAR*pi/(180*FPS)  # Radianes por fotograma
+
+# Se trata del uso de dos robots
+# Este es el robot ideal, es decir, el que se quiere igualar o con el que se debe de comparar
+ideal = robot()
+ideal.set_noise(0,0,.1)   # Ruido lineal / radial / de sensado
+ideal.set(*P_INICIAL)     # operador 'splat'
+
+# Este es el robot al que se le añade ruido para que se desvíe y se pueda implementar la localización
+real = robot()
+real.set_noise(.01,.01,.1)  # Ruido lineal / radial / de sensado
+real.set(*P_INICIAL)
+
+random.seed(0)
+tray_ideal = [ideal.pose()]  # Trayectoria percibida
+tray_real = [real.pose()]     # Trayectoria seguida
+
+tiempo  = 0.
+espacio = 0.
+#random.seed(0)
+random.seed(datetime.now())
+
+# Localizar inicialmente al robot (IMPORTANTE)
+
+
+for punto in objetivos:
+  while distancia(tray_ideal[-1],punto) > EPSILON and len(tray_ideal) <= 1000:
+    pose = ideal.pose()
+
+    w = angulo_rel(pose,punto)
+    if w > W:  w =  W
+    if w < -W: w = -W
+    v = distancia(pose,punto)
+    if (v > V): v = V
+    if (v < 0): v = 0
+
+    if HOLONOMICO:
+      if GIROPARADO and abs(w) > .01:
+        v = 0
+      ideal.move(w,v)
+      real.move(w,v)
+    else:
+      ideal.move_triciclo(w,v,LONGITUD)
+      real.move_triciclo(w,v,LONGITUD)
+    tray_ideal.append(ideal.pose())
+    tray_real.append(real.pose())
+
+    # Hay que especificar si la distancia real y la generada difiere, si difiere se aplica la localización
+
+    espacio += v
+    tiempo  += 1
+
+if len(tray_ideal) > 1000:
+  print ("<!> Trayectoria muy larga - puede que no se haya alcanzado la posicion final.")
+print ("Recorrido: "+str(round(espacio,3))+"m / "+str(tiempo/FPS)+"s")
+print ("Distancia real al objetivo: "+\
+    str(round(distancia(tray_real[-1],objetivos[-1]),3))+"m")
+mostrar(objetivos,tray_ideal,tray_real)  # Representaci�n gr�fica
+
